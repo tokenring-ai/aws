a3d1fc464d5eeb2ad661161da585b39c103a191b
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 12:50:36 -0700

    Implemented auto-configuration, that allows the package to automatically install into TokenRing apps

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {z} from "zod";
+import AWSService from "./AWSService.ts";
 import * as chatCommands from "./chatCommands.ts";
 import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 
-export {default as AWSService} from "./AWSService.ts";
+export const AWSConfigSchema = z.any().optional();
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
-  chatCommands,
-  tools
+  install(agentTeam: AgentTeam) {
+    const config = agentTeam.getConfigSlice('aws', AWSConfigSchema);
+    if (config) {
+      agentTeam.addTools(packageInfo, tools);
+      agentTeam.addChatCommands(chatCommands);
+      agentTeam.addServices(new AWSService(config));
+    }
+  }
 };
+
+export {default as AWSService} from "./AWSService.ts";
